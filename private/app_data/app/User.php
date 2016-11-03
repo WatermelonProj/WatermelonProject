d806aca67691905b4360618a910594afcb33c057
João Paulo Polles <joaopaulopolles@gmail.com>
2016-11-03 10:22:21 -0200

    Inserido e corrigido os principais controllers

--- private/app_data/app/User.php
+++ private/app_data/app/User.php
@@ -26,4 +26,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function receita() {
+      return $this->hasMany("App\Receita", 'idUsuario', 'idUsuario');
+    }
+    public function cardapio() {
+      return $this->hasMany("App\Cardapio","idUsuario", "idUsuario");
+    }
 }
